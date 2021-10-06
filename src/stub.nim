09b7b1bdc6546d0d64f4543ec6ee9443dfa4fe80
Leon Lysak <leon.l.lysak@gmail.com>
2021-10-05 20:46:27 -0400

    Add files via upload

---
+++ src/stub.nim
@@ -0,0 +1,10 @@
+## Import independent scene objects here
+## (no need to import everything, just independent roots)
+
+# IMPORT THE MODULES HERE:
+#
+#
+#
+
+when not defined(release):
+  import segfaults # converts segfaults into NilAccessError
---
+++ src/stub.nimble
@@ -0,0 +1,6 @@
+version       = "0.1.0"
+author        = "Niminem"
+description   = "Godot-Nim Project Stub"
+license       = "MIT"
+
+requires "godot >= 0.8.0"
